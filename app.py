7855d4262765b459ad4acbaa95e561fafaf90d21
Kogoon <gojs712@gmail.com>
2022-08-12 15:03:27 +0900

    Modified main page test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_simplelogin import SimpleLogin
 from flask_restful import Resource, Api
 
@@ -6,12 +6,28 @@ app = Flask(__name__)
 app.secret_key = '1q2w3e4r!'
 api = Api(app)
 
-
 class HelloWorld(Resource):
     def get(self):
         return {'hello' : 'world'}
 
-api.add_resource(HelloWorld, '/')
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+
+api.add_resource(HelloWorld, '/helloworld')
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template("index.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
