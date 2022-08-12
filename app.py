60d22123cabdc7896c2fef47b213a709239dbf9d
Kogoon <gojs712@gmail.com>
2022-08-12 12:26:38 +0900

    Flask Restful sample

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_simplelogin import SimpleLogin
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 app.secret_key = '1q2w3e4r!'
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello' : 'world'}
 
-SimpleLogin(app)
+api.add_resource(HelloWorld, '/')
 
+if __name__=="__main__":
+    app.run(debug=True)
