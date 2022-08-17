1243d4cb1e8894cc89c532418e3c23170624325d
Kogoon <gojs712@gmail.com>
2022-08-18 00:27:38 +0900

    AWS Elastic Cache - Redis Test

--- composetest/app.py
+++ composetest/app.py
@@ -4,7 +4,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='', port=6379)
 
 def get_hit_count():
     retries = 5
@@ -21,3 +21,7 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/test')
+def test():
+    return "Hello World!"
